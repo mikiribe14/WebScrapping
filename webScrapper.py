b56bebd5c37f141a51cca5eed24f9cd33ff3a37d
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-02-28 22:18:16 +0100

    get the prices OK

--- webScrapper.py
+++ webScrapper.py
@@ -11,8 +11,37 @@ def download_page():
 def searchOffers(page):
     tree = bs4.BeautifulSoup(page,"lxml")
     list=tree.find("ul","goodlist_1")
-    print(list)
+    products=list.find_all("li")
 
+    result=[]
+    for item in products:
+        thingsList=getOffers(item)
+        result.append(thingsList)
+
+    print(result)
+
+def getOffers(prod):
+    single=[]
+    actPrice = prod.find("span","price")
+    single.append(actPrice.text)
+
+    oldPrice=prod.find("span","price_old")
+    single.append(oldPrice.text)
+
+    #print(actPrice.text, oldPrice.text)
+
+    return single
+
+
+
+
+'''find all retorna una llista de "arbres", mentre
+find retorna un valor("un arbre")!!!!!!! '''
+
+def getOffers2(plist):
+    result=[]
+    for product in plist:
+        result.append()
 
 if __name__=="__main__":
     page = download_page()
