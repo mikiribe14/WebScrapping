237e9ff1f42e6e3293df7ca98947751723e6e761
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-03-01 11:30:12 +0100

    Final code commit

--- webScrapper.py
+++ webScrapper.py
@@ -2,7 +2,7 @@ import bs4
 from urllib.request import urlopen
 
 def download_page():
-#returns a url????? page
+#returns the Banggood offers page
     file=urlopen("https://www.banggood.com/Flashdeals.html")
     page=file.read()
     file.close()
@@ -23,26 +23,23 @@ def searchOffers(page):
 
 def getProductData(prod):
 #returns a list with the name, actual price, old price and % of discount of a product
-    single=[]
+    data=[]
     name=prod.find("span","title")
-    single.append(name.text)
-
+    data.append(name.text)
     actPrice = prod.find("span","price")
-    single.append(float(actPrice.text[3:]))
-
+    data.append(float(actPrice.text[3:]))
     oldPrice=prod.find("span","price_old")
-    single.append(float(oldPrice.text[3:]))
-
+    data.append(float(oldPrice.text[3:]))
     discount=prod.find("span","discount")
-    single.append(discount.text)
+    data.append(discount.text[:-4])
+    return data
 
-    return single
-
-
-
-
-'''find all retorna una llista de "arbres", mentre
-find retorna un valor("un arbre")!!!!!!! '''
+def printOffers(offers):
+#prints a message to the user explaining the offers
+    for offer in offers:
+        print("Hey, the product",offer[0],"is now for just", offer[1],
+        "$ !!. (It was for ",offer[2],"$ before). It's a dicount of",offer[3],
+         "don't you waste it!!!\n")
 
 if __name__=="__main__":
     page = download_page()
