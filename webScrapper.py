88caedd600be59f1e3f856e025ca571765925410
mikiribe14 <alcarras.miquel.ribes@gmail.com>
2019-03-04 22:06:09 +0100

    Sorting by discount added

--- webScrapper.py
+++ webScrapper.py
@@ -1,6 +1,9 @@
 import bs4
 from urllib.request import urlopen
 
+#specific library for order de offers by the percentatge of dicount.
+from operator import itemgetter
+
 def download_page():
 #returns the Banggood offers page
     file=urlopen("https://www.banggood.com/Flashdeals.html")
@@ -31,17 +34,18 @@ def getProductData(prod):
     oldPrice=prod.find("span","price_old")
     data.append(float(oldPrice.text[3:]))
     discount=prod.find("span","discount")
-    data.append(discount.text[:-4])
+    data.append(int(discount.text[:-5]))
     return data
 
 def printOffers(offers):
 #prints a message to the user explaining the offers
+    offers.sort(key=itemgetter(3),reverse=True);
     for offer in offers:
-        print("Hey, the product",offer[0],"is now for just", offer[1],
-        "$ !!. (It was for ",offer[2],"$ before). It's a dicount of",offer[3],
-         "don't you waste it!!!\n")
+        print("Hey, the product (",offer[0],") is now for just (", offer[1],
+        "$) !!. It was for (",offer[2],"$) before. It's a discount of (",offer[3],"% ).\n")
 
 if __name__=="__main__":
     page = download_page()
     offers=searchOffers(page)
+    offers.sort(key=itemgetter(3),reverse=True)
     printOffers(offers)
